45f07a90e70719fa23415311c00ce3277156e18f
George Stefan Chira <georgestefan.chira@gmail.com>
2023-07-28 02:57:33 +0100

    Reworked
    Changed comments, added exceptions so that it doesn't stop if a data point
    gets measured wrong, and added a whole class for local influxdb

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
-from tools import upload_data, read_data
+from tools import upload_data_influxdb_cloud, upload_data_local_influx, read_data
 import time, os
 
 if __name__== "__main__":
-    upload_model_to_cloud = upload_data(bucket="Solar Power",
-                                        org="B2Z",
-                                        token= os.environ.get("INFLUXDB_TOKEN"),
-                                        url="https://eu-central-1-1.aws.cloud2.influxdata.com") 
+    # send = upload_data_influxdb_cloud(bucket="Solar-Power",
+    #                                     org="B2Z",
+    #                                     token= os.environ.get("INFLUXDB_TOKEN"),
+    #                                     url="https://eu-central-1-1.aws.cloud2.influxdata.com") 
     
-    get_data_model = read_data()
+    send = upload_data_local_influx(ifuser="admin",
+                                    ifpass="you_thought_this_was_it",
+                                    ifdb="b2z",
+                                    ifhost="",
+                                    ifport= 8086) 
+ 
     
-    hash_netin1, hash_netout2 = 0.0, 0.0
-
+    
+    model = read_data()
+    
+    
+    # hold the last network usage
+    # the way the function works is by getting the total upload/download at the check
+    # so you take one measurement at start and then one every step, take the difference, send that
+    # and then save the last measurement. Now you have data from a whole loop, including sending the
+    # network data.
+    hash_netin1, hash_netout2 = model.measure_network()
+    
     for i in range(360):
-        start_time=time.time_ns()
-        
+        start_time=time.time_ns() 
         
         # net measuring part
-        net_in, net_out = get_data_model.measure_network()
-        
-        net_in  = (net_in-hash_netin1) /1024 /1024
-        net_out = (net_out-hash_netout2) / 1024 /1024
-        
-        hash_netin1, hash_netout2 =get_data_model.measure_network()
-        
-        upload_model_to_cloud.net_usage(net_in=net_in, net_out=net_out)
-        
+        net_in, net_out = model.measure_network()
+        send.net_usage(net_in=( (net_in-hash_netin1) /1024 /1024), net_out=((net_out-hash_netout2) / 1024 /1024))
+        hash_netin1, hash_netout2 = net_in, net_out
         
         # send cpu and ram data 
-        upload_model_to_cloud.log_cpu_data()
-        data = get_data_model.measure()
-        upload_model_to_cloud.log_generic(data=data,point_name="Electricity Gen",tag_name="House")
+        data = model.measure_cpu()
+        send.cpu_data(data)
         
+        # voltage from the model
+        data = model.fake_measure()
+        send.generic(data=data,point_name="Electricity Gen",tag_name="House")
         
-        
+
         end_time=time.time_ns()
-        duration=max(1,(10+(start_time-end_time)*1e-9)) 
-        print(duration, end_time, start_time)
+        # make sure it's every 10 seconds -ish
+        duration=max(1,(10+(start_time-end_time)*1e-9)) # 1e-9 because nanoseconds 
         time.sleep(duration)
         
